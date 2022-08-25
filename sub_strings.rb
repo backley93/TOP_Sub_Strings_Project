77aa051330a95ac747bfe6e2bde69affa7021089
Branden <branden.ackley@gmail.com>
2022-08-24 21:15:05 -0700

    created the dictionary of words and started method definition

---
+++ sub_strings.rb
@@ -0,0 +1,6 @@
+dictionary = ['i', 'want', 'to', 'be', 'the', 'very', 'best', 'like', 'no',
+'one', 'ever', 'was', 'catch', 'them', 'is', 'my', 'real', 'test', 'train',
+'cause']
+
+def substrings(string, words)
+end
