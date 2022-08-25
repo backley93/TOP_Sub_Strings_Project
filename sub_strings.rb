bdb912cabf69c08165d803be3e5637d457cd9b7c
Branden <branden.ackley@gmail.com>
2022-08-25 10:58:27 -0700

    Finished method by adding in conditonals to increment key[value] in the hash

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,4 +3,19 @@ dictionary = ['i', 'want', 'to', 'be', 'the', 'very', 'best', 'like', 'no',
 'cause']
 
 def substrings(string, words)
+  string = string.downcase
+  array = string.split
+  times_in_string = Hash.new
+  array.each do |element|
+    words.each do |word|
+      if element == word
+        if times_in_string[element] == nil
+          times_in_string[element] = 1
+        else
+          times_in_string[element] += 1
+        end
+      end
+    end
+  end
+  p times_in_string
 end
